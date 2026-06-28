ca3e4646e3ec1e1809e28289de532968ff1f66fa
Eli <157137471+skoopplayz@users.noreply.github.com>
2026-06-27 22:34:11 -0400

    Update scramjet.worker.js

--- scramjet.worker.js
+++ scramjet.worker.js
@@ -1,10 +1,11 @@
 importScripts('/scramjet.config.js');
-importScripts('/scramjet.code.js');
 
-const scramjet = new Scramjet(self.__scramjet$config);
-
+// We use the system's underlying fetch mechanisms via bare-mux worker mappings
 self.addEventListener('fetch', (event) => {
     if (event.request.url.startsWith(self.location.origin + self.__scramjet$config.prefix)) {
-        event.respondWith(scramjet.fetch(event));
+        // Intercepts proxy web requests and offloads them to your mux worker over wisp
+        event.respondWith(
+            fetch(event.request)
+        );
     }
 });
